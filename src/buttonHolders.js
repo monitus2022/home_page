1b7d024924d48b67a3cdc5749291c8709ab08c45
TC <monitus202201@proton.me>
2022-10-02 18:14:07 +0800

    tidy up paths

--- src/buttonHolders.js
+++ src/buttonHolders.js
@@ -1,26 +1,23 @@
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import './buttonHolders.css';
+import ImagePaths from './imagePaths';
 
 const ButtonHolders = () => {
+
+    const buttons = ImagePaths.map((imagePath) => {
+        return (
+            <Grid item xs={6} sm={6} md={6} lg={6} className="imageHolder">
+                <img src={imagePath.src} alt={imagePath.alt} title={imagePath.title} />
+            </Grid>)
+    });
 
     return (
         <div className="buttonHolders">
             <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={6} sm={6} md={6} lg={6} className="imageHolder">
-                    <img src="https://yt3.ggpht.com/ytc/AMLnZu_dL-8b7yvrrbtpkRNShFE440mdUd74vIQpvfXujw=s900-c-k-c0x00ffffff-no-rj" alt="pad" title="パズドラ"/>
+                <Grid container spacing={2}>
+                    {buttons}
                 </Grid>
-                <Grid item xs={6} sm={6} md={6} lg={6} className="imageHolder">
-                    <img src="https://play-lh.googleusercontent.com/4an7wZ3kRBKwdirzXSgL2W3foMhfQmsNc73vX2WQtx8hGJpBEJHoGmMMWXsZMpXGY-U" alt="fgo" title="Fate/ Grand Order"/>
-                </Grid>
-                <Grid item xs={6} sm={6} md={6} lg={6} className="imageHolder">
-                    <img src="https://www.pngitem.com/pimgs/m/117-1173386_meteor-clipart-final-fantasy-final-fantasy-14-png.png" alt="ffxiv" title="Final Fantaxy 14"/>
-                </Grid>
-                <Grid item xs={6} sm={6} md={6} lg={6} className="imageHolder">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/1200px-Icon-round-Question_mark.svg.png" alt="info" title="My info"/>
-                </Grid>
-            </Grid>
             </Box>
         </div>
     )
---
+++ src/imagePaths.js
@@ -0,0 +1,24 @@
+const ImagePaths = [
+    {
+        title: "パズドラ",
+        alt: "パズドラ",
+        src: "https://yt3.ggpht.com/ytc/AMLnZu_dL-8b7yvrrbtpkRNShFE440mdUd74vIQpvfXujw=s900-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        title: "Fate/ Grand Order",
+        alt: "Fate/ Grand Order",
+        src: "https://play-lh.googleusercontent.com/4an7wZ3kRBKwdirzXSgL2W3foMhfQmsNc73vX2WQtx8hGJpBEJHoGmMMWXsZMpXGY-U"
+    },
+    {
+        title: "Final Fantaxy 14",
+        alt: "Final Fantaxy 14",
+        src: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/bc2a2cbe-29f7-464d-8396-131bdd4ccd3f/d7xn4e7-531cd7bf-e2e4-40f2-88e7-ba524acd0803.png"
+    },
+    {
+        title: "My info",
+        alt: "My info",
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/1200px-Icon-round-Question_mark.svg.png"
+    }
+];
+
+export default ImagePaths;
