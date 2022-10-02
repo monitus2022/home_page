080d8dd5b6853e0fcf0767a14f16adec190a9011
TC <monitus202201@proton.me>
2022-10-02 17:24:37 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ButtonHolders from './buttonHolders';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{textAlign: 'center', borderStyle: "dotted"}}>Monitus.org</h1>
+        <ButtonHolders />
+      </header>
+    </div>
+  );
+}
+
+export default App;
